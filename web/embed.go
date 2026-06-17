81d5b29a582769283892716e57ee829c7e92c1a3
aaalzk <368403+aaalzk@user.noreply.gitee.com>
2026-06-17 13:08:19 +0800

    Fix embed usage: use embed.FS type

--- web/embed.go
+++ web/embed.go
@@ -1,16 +1,18 @@
 package web
 
 import (
+	"embed"
 	"net/http"
 )
 
 //go:embed static/index.html
-var indexHTML []byte
+var staticFS embed.FS
 
 // Handler returns an http.Handler that serves the single-page dashboard.
 func Handler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data, _ := staticFS.ReadFile("static/index.html")
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.Write(indexHTML)
+		w.Write(data)
 	})
 }
